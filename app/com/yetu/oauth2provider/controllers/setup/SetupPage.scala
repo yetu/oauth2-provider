649b5efd0696c10b2469f4df3824f5170218b763
Matthias Heyder <matthias.heyder@meelogic.com>
2015-03-12 16:01:42 +0100

    WIP new registration page

--- app/com/yetu/oauth2provider/controllers/setup/SetupPage.scala
+++ app/com/yetu/oauth2provider/controllers/setup/SetupPage.scala
@@ -3,9 +3,13 @@ package com.yetu.oauth2provider.controllers.setup
 import play.api.mvc._
 import com.yetu.oauth2provider.views
 
-object DownloadPage extends Controller {
+object SetupPage extends Controller {
 
   val download = Action {
     Ok(views.html.download.render())
+  }
+
+  val registration = Action {
+    Ok(views.html.registration.render())
   }
 }
