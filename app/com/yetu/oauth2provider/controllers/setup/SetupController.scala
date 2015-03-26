0ef71df2b53cb1e5a85acd1b6ca680120f2da8f5
Elisa Hilprecht <elisa.hilprecht@yetu.com>
2015-03-26 16:06:37 +0100

    Start signup for setup with a form.

---
+++ app/com/yetu/oauth2provider/controllers/setup/SetupController.scala
@@ -0,0 +1,30 @@
+package com.yetu.oauth2provider.controllers.setup
+
+import com.yetu.oauth2provider.oauth2.models.YetuUser
+import com.yetu.oauth2provider.utils.Config.FrontendConfiguration
+import play.api.mvc._
+import com.yetu.oauth2provider.views
+import securesocial.controllers.{ BaseRegistration }
+import securesocial.core.{RuntimeEnvironment}
+
+class SetupController(override implicit val env: RuntimeEnvironment[YetuUser]) extends BaseRegistration[YetuUser] {
+
+  override def startSignUp = {
+    Action {
+      implicit request => Ok(views.html.setup.startSignUpForSetup(form))
+    }
+  }
+
+  val download = Action {
+    Ok(views.html.setup.download(FrontendConfiguration.setupDownloadUrlMac, FrontendConfiguration.setupDownloadUrlWin))
+  }
+
+  val confirmmail = Action {
+    Ok(views.html.setup.confirmmail.render())
+  }
+
+  val confirmedmail = Action {
+    Ok(views.html.setup.confirmedmail.render())
+  }
+
+}
