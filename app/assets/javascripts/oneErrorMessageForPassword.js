63c4bd261b8be1fa2d34981b8b049ff39fc1faa3
Elisa Hilprecht <elisa.hilprecht@yetu.com>
2015-04-14 14:23:40 +0200

    Put only showing one password error message to password validator in backend.

--- app/assets/javascripts/oneErrorMessageForPassword.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by elisahilprecht on 13/04/15.
- */
-(function(){
-  var errorElement = document.getElementById('password1IDErrorText');
-  if(errorElement!=undefined){
-    errorElement.innerHTML = errorElement.innerHTML.split(',')[0];
-  }
-})();
