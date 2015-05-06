62a169a83c28f762bf9a8ce232f67c9d87b782e2
Lars Blumberg <lars.blumberg@yetu.com>
2015-05-06 18:01:16 +0200

    Show input hints on hover and focus

--- app/assets/javascripts/form-validator.js
+++ app/assets/javascripts/form-validator.js
@@ -10,24 +10,51 @@ var FormValidator = function(root) {
         // Do not show placeholder if input has an error as error text will cover it
         input.placeholderCopy = input.placeholder;
         input.placeholder = '';
-        inputError.addEventListener('click', this.onInputErrorClick.bind(this, input, inputError));
+        inputError.addEventListener('click', this.onInputErrorClick.bind(this, input));
+      }
+      // Initialize input's help icon
+      var inputHintIcon = root.getElementById(input.id + 'HelpIcon');
+      var inputHintText = root.getElementById(input.id + 'HelpText');
+      if (inputHintIcon && inputHintText) {
+        inputHintIcon.addEventListener('mouseover', this.onInputHintMouseOver.bind(this, inputHintText));
+        inputHintIcon.addEventListener('mouseout', this.onInputHintMouseOut.bind(this, input, inputHintText));
       }
       // Initialize input
-      input.addEventListener('click', this.onInputClick.bind(this, input, inputError));
-      input.addEventListener('focus', this.onInputClick.bind(this, input, inputError));
+      input.addEventListener('focus', this.onInputFocus.bind(this, input, inputError, inputHintText));
+      input.addEventListener('blur', this.onInputBlur.bind(this, inputHintText));
     }
   };
 
-  this.onInputClick = function(input, inputError) {
+  this.onInputFocus = function(input, inputError, inputHintText) {
     if (inputError) {
       inputError.classList.add('display-none');
       // Restore placeholder text that was removed to show the error text instead
       input.placeholder = input.placeholderCopy;
     }
+    if (inputHintText) {
+      inputHintText.classList.remove('display-none');
+    }
   };
 
-  this.onInputErrorClick = function(input, inputError) {
+  this.onInputBlur = function(inputHintText) {
+    if (inputHintText) {
+      inputHintText.classList.add('display-none');
+    }
+  };
+
+  this.onInputErrorClick = function(input) {
     input.focus();
+  };
+
+  this.onInputHintMouseOver = function(inputHintText) {
+    inputHintText.classList.remove('display-none');
+  };
+
+  this.onInputHintMouseOut = function(input, inputHintText) {
+    // Hide hint if input isn't focused
+    if (input !== document.activeElement) {
+      inputHintText.classList.add('display-none');
+    }
   };
 
   this.init();
