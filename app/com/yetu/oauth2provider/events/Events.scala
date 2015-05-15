f5d2319515f63b6915fe99f0d5b8dae73996a33d
azadorozhniy <artem.zadorozhniy@yetu.com>
2015-05-15 12:08:45 +0200

    Moved logoutEvent to config

--- app/com/yetu/oauth2provider/events/Events.scala
+++ app/com/yetu/oauth2provider/events/Events.scala
@@ -4,12 +4,12 @@ import com.yetu.notification.client.NotificationManager
 import com.yetu.notification.client.model.RabbitMessage
 import com.yetu.oauth2provider.oauth2.handlers.AuthorizationHandler
 import com.yetu.oauth2provider.oauth2.models.YetuUser
+import com.yetu.oauth2provider.utils.Config.YetuMessageEvents
 import com.yetu.oauth2provider.utils.NamedLogger
-import play.api.{Application, Logger}
+import play.api.{ Application, Logger }
 import play.api.libs.json._
-import play.api.mvc.{RequestHeader, Session}
-import securesocial.core.{Event, EventListener, LogoutEvent}
-
+import play.api.mvc.{ RequestHeader, Session }
+import securesocial.core.{ Event, EventListener, LogoutEvent }
 
 // TODO replace it with reading information form the RabbitMessage header
 // in the yetu-notification-client library
@@ -23,23 +23,20 @@ class LogoutEventListener(app: Application, authorizationHandler: AuthorizationH
 
   override def onEvent(event: Event[YetuUser], request: RequestHeader, session: Session): Option[Session] = {
     event match {
-      case LogoutEvent(user) => {
+      case LogoutEvent(user) =>
         broadcastLogout(user)
-      }
-      case otherEvent => {
+      case otherEvent =>
         Logger.trace("traced %s event for user %s".format(otherEvent, event.user))
-      }
     }
     None
   }
 
   def broadcastLogout(user: YetuUser) = {
-    //TODO: use config
-    val LOGOUT_EVENT_NAME = "logout"
+    val LOGOUT_EVENT_NAME = YetuMessageEvents.logoutEventName
+    val currentClientId = YetuMessageEvents.clientId
 
-    // dummy clientId here
     // TODO please change it and take information from topic into account
-    NotificationManager.publisherActor ! RabbitMessage(s"${user.uid}.oauth2provider.$LOGOUT_EVENT_NAME",
+    NotificationManager.publisherActor ! RabbitMessage(s"${user.uid}.$currentClientId.$LOGOUT_EVENT_NAME",
       Json.toJson(EventLogoutMessage(LOGOUT_EVENT_NAME, user.uid)).toString()
     )
   }
